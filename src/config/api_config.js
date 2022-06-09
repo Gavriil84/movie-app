c3c3ed3e2c6cf6cf77923f35387c1f82e33b7875
Gavriil84 <gabwt.84@gmail.com>
2022-06-08 17:02:56 -0700

    Optimize api codes

--- src/config/api_config.js
+++ src/config/api_config.js
@@ -1,5 +1,2 @@
-export const API_KEY = '67aa2811a423de1c7175bd5409417bfc'
-export const NOW_PLAYING = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
-export const POPULAR = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-export const TOP_RATED = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
-export const UPCOMING = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+export const API_KEY = '?api_key=67aa2811a423de1c7175bd5409417bfc&language=en-US&page=1'
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/'
