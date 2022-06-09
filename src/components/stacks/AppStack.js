caa7be7e3d090db935a4819348a2d017a1d58c1e
Gavriil84 <gabwt.84@gmail.com>
2022-06-08 17:02:40 -0700

    Set up stack screen for details screen

--- src/components/stacks/AppStack.js
+++ src/components/stacks/AppStack.js
@@ -1,27 +1,43 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import React from 'react'
 import IndexScreen from '../screens/IndexScreen';
+import React from 'react'
+import ShowScreen from '../screens/ShowScreen';
 
 const Stack = createNativeStackNavigator();
 
 const AppStack = () => (
 
-    <Stack.Navigator>
-        <Stack.Screen
-            name="Movies App"
-            component={IndexScreen}
-            options={{
-                title: 'Movies App',
-                headerStyle: {
-                    backgroundColor: '#2c3e50'
-                },
-                headerTitleStyle: {
-                    color: '#fff'
-                }
-            }}
-        />
-    </Stack.Navigator>
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Movies App"
+                component={IndexScreen}
+                options={{
+                    title: 'Movies App',
+                    headerStyle: {
+                        backgroundColor: '#2c3e50'
+                    },
+                    headerTitleStyle: {
+                        color: '#fff'
+                    },
+                }}
+            />
+            <Stack.Screen
+                name='Details'
+                component={ShowScreen}
+                options={({ route }) => ({
+                    title: route.params.title,
+                    id: route.params.id,
+                    release_date: route.params.release_date,
+                    popularity: route.params.popularity,
+                    overview: route.params.overview,
+                    image: route.params.image,
+                    headerBackTitle: 'Back to List'
+                })}
+            />
+        </Stack.Navigator>
+    </NavigationContainer>
 )
 
 
