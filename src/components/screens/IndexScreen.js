1471fba7ee59d0db3438d172062c07d6019f53e3
Gavriil84 <gabwt.84@gmail.com>
2022-06-08 17:01:04 -0700

    Migrate code to home component

--- src/components/screens/IndexScreen.js
+++ src/components/screens/IndexScreen.js
@@ -1,26 +1,5 @@
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
-import React from 'react'
-import MoviesContainer from '../containers/MoviesContainer';
-import SearchResults from '../containers/SearchResults';
-import TvShows from '../containers/TvShows';
+import Home from "../containers/Home";
 
-const Tab = createMaterialTopTabNavigator();
-
-const IndexScreen = () => {
-    return (
-
-        <Tab.Navigator
-            screenOptions={{
-                tabBarLabelStyle: { fontSize: 14, textTransform: 'none' },
-                tabBarIndicatorStyle: { backgroundColor: "#2c3e50" },
-            }}
-        >
-            <Tab.Screen name="Movies" component={MoviesContainer} />
-            <Tab.Screen name="Search Results" component={SearchResults} />
-            <Tab.Screen name="TV Shows" component={TvShows} />
-        </Tab.Navigator>
-
-    )
-}
+const IndexScreen = ({ navigation }) => <Home navigation={navigation} />
 
 export default IndexScreen
