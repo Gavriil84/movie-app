f881c08ab019ba27090771bd752cb1a2bd27ec5f
Gavriil84 <gabwt.84@gmail.com>
2022-06-09 21:05:04 -0700

    Optimize data fetch for different patterns

--- src/components/listitems/MovieCard.js
+++ src/components/listitems/MovieCard.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Box, Button, HStack, Image, VStack, Text } from 'native-base'
 import { useNavigation } from '@react-navigation/native'
+import placeholder from '../../../assets/placeholder.png'
 
 const MovieCard = (props) => {
-    const { image, title, popularity, release_date, id, overview } = props
+    const { image, title, popularity, release_date, id, searchType, media_type } = props
     const navigation = useNavigation();
 
     return (
         <Box mb={4} borderBottomWidth="1" borderColor="coolGray.300">
             <HStack>
                 <Box>
-                    <Image source={{ uri: image }} alt='movie image' size='lg' ml={5} mr={3} />
+                    {
+                        image == 'https://image.tmdb.org/t/p/original/null' || undefined ?
+                            (<Image source={placeholder} alt='movie image' size='lg' ml={5} mr={3} />) :
+
+                            (<Image source={{ uri: image }} alt='movie image' size='lg' ml={5} mr={3} />)
+                    }
                 </Box>
                 <Box width='60%'>
                     <VStack>
@@ -20,7 +26,7 @@ const MovieCard = (props) => {
                         <Button
                             bgColor="lightBlue.500"
                             mb={3}
-                            onPress={() => navigation.navigate('Details', { title: title, id: id, overview: overview, popularity: popularity, release_date: release_date, image: image })}>More Details</Button>
+                            onPress={() => navigation.navigate('Details', { title: title, id: id, image: image, searchType: searchType, media_type: media_type })}>More Details</Button>
                     </VStack>
                 </Box>
             </HStack>
